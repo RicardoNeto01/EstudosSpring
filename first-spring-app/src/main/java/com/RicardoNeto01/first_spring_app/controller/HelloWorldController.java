512c8b49efcf79fdbd64e77641e3e96b463f61c7
Ricardo Neto <ricardo123neto88@gmail.com>
2025-04-07 22:17:51 -0300

    feat: add HelloWorldController in package controller

---
+++ first-spring-app/src/main/java/com/RicardoNeto01/first_spring_app/controller/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.RicardoNeto01.first_spring_app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Podemos ter API STATEFULL: estado de cada cliente (autenticação, token, etc), é salvo no servidor.
+// E também API STATELESS: a cada requisição, é necessário receber todas as informações para executar a funcionalidade que o cliente quer.
+
+//Anotação para o Spring entender que essa classe é um controller.
+@RestController
+//Anotação para mapear os endpoints e qual requisição essa classe ira responder
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+
+    //Anotação para especificar qual requisição http esse método responde
+    // post, get, put, delete, patch, etc...
+
+    //GET /hello-world
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
