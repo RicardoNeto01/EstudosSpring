de172ff6d219fdc5a4b1d8bffdaf5daaae5a7c46
Ricardo Neto <ricardo123neto88@gmail.com>
2025-04-07 22:39:28 -0300

    feat: add HelloWorldService in package service

--- first-spring-app/src/main/java/com/RicardoNeto01/first_spring_app/service/HelloWorldService.java
+++ first-spring-app/src/main/java/com/RicardoNeto01/first_spring_app/service/HelloWorldService.java
@@ -1,6 +1,13 @@
 package com.RicardoNeto01.first_spring_app.service;
 
+import org.springframework.stereotype.Service;
 
-
+//Indica pro spring que isso é uma classe service(trata toda a lógica de negócio)
+@Service
 public class HelloWorldService {
+
+    public String helloWorld(String name){
+        return "Hello World " + name;
+    }
+
 }
