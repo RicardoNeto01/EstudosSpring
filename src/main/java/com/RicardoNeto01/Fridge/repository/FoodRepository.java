eb0fab1bdc798d429df4e18a322e06ec52ecfa20
Ricardo Neto <133607688+RicardoNeto01@users.noreply.github.com>
2025-04-08 22:11:39 -0300

    Delete src directory

--- src/main/java/com/RicardoNeto01/Fridge/repository/FoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.RicardoNeto01.Fridge.repository;
-
-import com.RicardoNeto01.Fridge.model.Food;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//Para funcionar corretamente, é necessário passsar o tipo de model utilizado, e o tipo do id que o model usa.
-public interface FoodRepository extends JpaRepository<Food, Long> {
-}
